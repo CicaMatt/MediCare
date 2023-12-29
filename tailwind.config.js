7e0a884fcd6a50021b4cebe12803db0c12fbbcd7
gianlucapalumbo0 <g.palumbo40@studenti.unisa.it>
2023-12-29 10:47:37 +0100

    Creazione Login

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./flaskDir/templates/*.html'],
+  theme: {
+    extend: {},
+    colors: {
+      'navbarreg': '#B5CBE8',
+    },
+  },
+  plugins: [],
+
+}
