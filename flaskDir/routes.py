027c0efc71296493b45679f486a863449b3430ea
gianlucapalumbo0 <g.palumbo40@studenti.unisa.it>
2024-01-07 11:28:04 +0100

    Inserimento carte

--- flaskDir/routes.py
+++ flaskDir/routes.py
@@ -171,10 +171,7 @@ def storiaMediCare():
 def impostazioni():
     return render_template("Impostazioni.html")
 
-@app.route('/metodopagamento')
-# @login_required and user is ente
-def metodopagamento():
-    return render_template("MetodoPagamento.html")
+
 
 if __name__ == '__main__':
     app.run()
