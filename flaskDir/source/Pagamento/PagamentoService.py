8c95df582d0662a26365911c9fbcdb7ee57fdeee
gianlucapalumbo0 <g.palumbo40@studenti.unisa.it>
2024-01-08 11:08:54 +0100

    Inserimento metodo creazione carta di credito

--- flaskDir/source/Pagamento/PagamentoService.py
+++ flaskDir/source/Pagamento/PagamentoService.py
@@ -19,6 +19,15 @@ class PagamentoService:
         db.session.commit()
 
 
+    @classmethod
+    def creaMetodo(cls, metodopagamento, cf):
+        cls.listaCarte = cls.getMetodi(cls, cf)
+        cls.listaCarte.append(metodopagamento)
+
+
+
+
+
 
 
 
