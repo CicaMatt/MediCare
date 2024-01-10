5e76047db86c84a6bfbd9b581c495dccd460469e
PRIMOVINICIOCALABRESE <p.calabrese17@studenti.unisa.it>
2024-01-10 19:31:30 +0100

    eliminazione paziente

--- flaskDir/source/Utente/AreaUtenteControl.py
+++ flaskDir/source/Utente/AreaUtenteControl.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, render_template, session, request
 from flask_login import current_user, login_required
 
+from flaskDir.source.prenotazioni import services
 from flaskDir.source.prenotazioni.services import PrenotazioneService, FascicoloService
+
 
 areautente_blueprint = Blueprint('areautente', __name__)
 
@@ -18,3 +20,10 @@ def storico():
 def getFascicolobyUtente():
     paziente = request.args.get('paziente')
     return render_template("FascicoloElettronico.html",listaDOC= FascicoloService.getDocumentiSanitari(paziente).all())
+
+@areautente_blueprint.route('/eliminapaziente',methods=['GET','POST'])
+@login_required
+def eliminaPaziente():
+    user = session.get('current_user.CF')
+    return services.PazienteService.eliminaPaziente(user)
+
