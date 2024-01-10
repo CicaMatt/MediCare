e0fe072c26dc7241134bde454558404a795a82c3
PRIMOVINICIOCALABRESE <p.calabrese17@studenti.unisa.it>
2024-01-10 21:08:13 +0100

    elimina medico

--- flaskDir/source/prenotazioni/MedicoControl.py
+++ flaskDir/source/prenotazioni/MedicoControl.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, session, request, jsonify
 from flask_login import current_user, login_required
 
+from flaskDir.source.prenotazioni import services
 from flaskDir.source.prenotazioni.MedicoService import MedicoService
 
 medico_blueprint = Blueprint('medico', __name__)
@@ -13,3 +14,16 @@ def getListaPazienti():
     dottore=request.args.get('dottore')
     return render_template("ListaPazienti.html",lista= MedicoService.getPazienti(dottore))
 
+@medico_blueprint.route('/eliminamedico',methods=['GET','POST'])
+@login_required
+def eliminaMedico():
+    email = current_user.email
+    if email is None:
+        return jsonify({"error": "Medico non trovato nella sessione"}), 400
+
+    success = services.MedicoService.rimuoviMedico(email)
+
+    if success:
+        return jsonify({"message": "Paziente eliminato con successo"})
+    else:
+        return jsonify({"error": "Errore durante l'eliminazione del medico"}), 500
