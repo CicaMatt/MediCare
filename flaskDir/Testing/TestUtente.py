87c074da163e4bafe6312bfc4955d9b582c39255
MatteoAV02 <m.avella17@studenti.unisa.it>
2024-01-14 19:07:27 +0100

    test utente

--- flaskDir/Testing/TestUtente.py
+++ flaskDir/Testing/TestUtente.py
@@ -6,30 +6,43 @@ from sqlalchemy_utils import database_exists, create_database
 from urllib.parse import quote
 from flaskDir import app, db
 from flaskDir.MediCare.model.entity.DocumentoSanitario import DocumentoSanitario
+from flaskDir.MediCare.model.entity.MetodoPagamento import MetodoPagamento
 from flaskDir.MediCare.model.entity.Paziente import Paziente
+from flaskDir.source.Pagamento.PagamentoService import PagamentoService
 from flaskDir.source.Utente.ISEEService import ISEEService
 from flaskDir.source.prenotazioni.services import FascicoloService
 
 
 #pip install mysqlclient
 
-@pytest.fixture(autouse=True)
-def setUp():
+@pytest.fixture(autouse=True, scope='session')
+def setUp(request):
     # Configura il database di test
-    app.config['SQLALCHEMY_DATABASE_URI_TEST'] = f"mysql+pymysql://root:{quote('Cancello1@')}@localhost:3306/testmedicare"
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS_TEST"] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://root:{quote('Cancello1@')}@localhost:3306/testmedicare"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["TESTING"] = True
+    db.init_app(app)
     # Crea le tabelle del database di test
-    with app.app_context():
-        db.drop_all()
-        if not database_exists(app.config["SQLALCHEMY_DATABASE_URI_TEST"]):
-            create_database(app.config["SQLALCHEMY_DATABASE_URI_TEST"])
+    with app.test_client():
+        with app.app_context():
+            #db.drop_all()
 
-        db.create_all()
+            if not database_exists(app.config["SQLALCHEMY_DATABASE_URI"]):
+                create_database(app.config["SQLALCHEMY_DATABASE_URI"])
+
+            db.create_all()
+
+        def teardown():
+            # Tuo codice di tearDown qui
+            with app.app_context():
+                db.session.close_all()
+                db.drop_all()
+
+        # Aggiungi la funzione di tearDown alla richiesta
+        request.addfinalizer(teardown)
 
 
 def test_addDocumento():
-    app.config['SQLALCHEMY_DATABASE_URI'] = app.config['SQLALCHEMY_DATABASE_URI_TEST']
-
     with app.app_context():
         mario = Paziente()
         mario.nome = "Mario"
@@ -37,7 +50,7 @@ def test_addDocumento():
         mario.email = "mariorossi@gmail.com"
         mario.password_hash = "cancello"
         mario.chiaveSPID = 10
-        mario.ISEE_ordinario = 10000
+        mario.ISEE_ordinario = None
         mario.CF = "PROVAS029DGH6712"
         mario.cellulare = "3312258345"
         mario.luogoNascita = "Salerno"
@@ -67,9 +80,6 @@ def test_addDocumento():
         assert documento.descrizione == 'test'
         assert  documento.tipo == 'Ricetta'
 
-        db.session.delete(paziente)
-        db.session.delete(documento)
-        db.session.commit()
 
 
 def test_getDocumentiSanitari():
@@ -77,8 +87,10 @@ def test_getDocumentiSanitari():
         paziente_test = Paziente(CF='PROVAS029DGH6712', chiaveSPID=123, nome='Mario', cognome='Rossi',
                                  email='mariorossi@gmail.com', password_hash='mario', cellulare='3457895647',
                                  domicilio='Salerno', dataNascita='2000-12-31', luogoNascita='Salerno', sesso='maschio',
-                                 ISEE_ordinario=23456)
-        db.session.add(paziente_test)
+                                 ISEE_ordinario=None)
+        # Senza inserirlo nel db
+        # db.session.add(paziente_test)
+        # db.session.commit()
         cf = paziente_test.CF
 
         listaDocumenti = FascicoloService.getDocumentiSanitari(cf)
@@ -91,26 +103,15 @@ def test_getDocumentiSanitari():
 
 
 
-        # Elimina il paziente
-        # Paziente.query.filter_by(CF='PROVAS029DGH6712').delete()
-        # db.session.commit()
-
-
 def test_changeISEE():
     with app.app_context():
-        paziente_test = Paziente(CF='PROVAS029DGH6712', chiaveSPID=123, nome='Mario', cognome='Rossi',
-                                 email='mariorossi@gmail.com', password_hash='mario', cellulare='3457895647',
-                                 domicilio='Salerno', dataNascita='2000-12-31', luogoNascita='Salerno', sesso='maschio',
-                                 ISEE_ordinario=None)
-        db.session.add(paziente_test)
-        cf = paziente_test.CF
+
+
+        cf = 'PROVAS029DGH6712'
         new = 10000
 
         ISEEService.changeISEE(cf,new)
 
-        assert paziente_test.ISEE_ordinario is not None
-        assert paziente_test.ISEE_ordinario == new
-
-        # Elimina il paziente
-        # Paziente.query.filter_by(CF='PROVAS029DGH6712').delete()
-        # db.session.commit()
+        paziente = Paziente.query.filter_by(CF=cf).first()
+        assert paziente.ISEE_ordinario is not None
+        assert paziente.ISEE_ordinario == new
