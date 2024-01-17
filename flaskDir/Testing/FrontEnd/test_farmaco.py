adfaaf8fa69cebbce38d73b1b877661b9193dfda
Giodvk <giovannicasaburi02@gmail.com>
2024-01-17 11:09:39 +0100

    Testing di sistema Farmaci

--- flaskDir/Testing/FrontEnd/test_farmaco.py
+++ flaskDir/Testing/FrontEnd/test_farmaco.py
@@ -3,23 +3,33 @@ import pytest
 import time
 import json
 from selenium import webdriver
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service as ChromeService
+from flaskDir.MediCare.model.entity.Farmaco import Farmaco
+
+from flaskDir import app, db
 
 
 class TestFarmaco():
-    def setup_method(self, method):
+    def setup_method(self):
         self.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
         self.vars = {}
 
-    def teardown_method(self, method):
+        with app.app_context():
+            farmaco=Farmaco(ID=10, prezzo=5.50, nome="Augmentin", categoria="Compresse", descrizione="Antibiotico per l'influenza alta", immagine="testing.png")
+            db.session.add(farmaco)
+            db.session.commit()
+
+    def teardown_method(self):
         self.driver.quit()
+        with app.app_context():
+            db.session.delete(Farmaco.query.filter_by(ID=10).first())
+            db.session.commit()
 
     def test_farmaco(self):
         self.driver.get("http://127.0.0.1:5000/")
@@ -42,3 +52,27 @@ class TestFarmaco():
         self.driver.find_element(By.CSS_SELECTOR, ".bg-blue-600").click()
 
         assert self.driver.current_url=="http://127.0.0.1:5000/farmacia/filtroCatalogo"
+
+    def test_farmacoNoFind(self):
+        self.driver.get("http://127.0.0.1:5000/")
+        self.driver.set_window_size(1920, 1032)
+        self.driver.find_element(By.LINK_TEXT, "Farmaci").click()
+        self.driver.find_element(By.ID, "search").click()
+        self.driver.find_element(By.ID, "search").send_keys("benactiveGola")
+        self.driver.find_element(By.ID, "search").send_keys(Keys.ENTER)
+        error=WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.ID,"noPresent")))
+        assert error
+        assert error.text=="Ci dispiace ma questo farmaco non è presente nel nostro archivio di farmaci"
+
+    def test_farmacoFind(self):
+        self.driver.get("http://127.0.0.1:5000/")
+        self.driver.set_window_size(1920, 1032)
+        self.driver.find_element(By.LINK_TEXT, "Farmaci").click()
+        self.driver.find_element(By.ID, "search").click()
+        self.driver.find_element(By.ID, "search").send_keys("Augmentin")
+        self.driver.find_element(By.ID, "search").send_keys(Keys.ENTER)
+        self.driver.find_element(By.CSS_SELECTOR, ".h-48").click()
+        farmaco=WebDriverWait(self.driver, 10).until(EC.presence_of_element_located((By.ID,"nomeFarmaco")))
+        assert self.driver.current_url=="http://127.0.0.1:5000/farmacia/dettagliFarmaco?id=10&categoria=Compresse"
+        assert farmaco
+        assert farmaco.text=="Augmentin"
